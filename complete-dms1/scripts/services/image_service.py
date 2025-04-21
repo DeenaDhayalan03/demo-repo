573646938b95e388a676fd03de3a0094061bc047
DeenadhayalanM <deenadhayalan.m@corp.knowledgelens.com>
2025-04-22 01:01:09 +0530

    committed

--- complete-dms1/scripts/services/image_service.py
+++ complete-dms1/scripts/services/image_service.py
@@ -11,43 +11,43 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 image_router = APIRouter()
 
 
-@docker_router.post(Endpoints.IMAGE_BUILD_ADVANCED)
+@image_router.post(Endpoints.IMAGE_BUILD_ADVANCED)
 def build_image_service(data: ImageBuildRequest, token: str = Depends(oauth2_scheme)):
     logger.info(f"User is attempting to build an image with tag: {data.tag}")
     return build_image(data, token)
 
 
-@docker_router.post(Endpoints.IMAGE_BUILD_FROM_GITHUB)
+@image_router.post(Endpoints.IMAGE_BUILD_FROM_GITHUB)
 def build_image_from_github_service(data: ImageGithubBuildRequest, token: str = Depends(oauth2_scheme)):
     logger.info(f"User is attempting to build an image from GitHub repository: {data.github_url}")
     return build_image_from_github(data, token)
 
 
-@docker_router.get(Endpoints.IMAGE_LIST)
+@image_router.get(Endpoints.IMAGE_LIST)
 def list_images_service(name: str = None, all: bool = False, filters: dict = None, token: str = Depends(oauth2_scheme)):
     logger.info(f"User is listing Docker images with filters: {filters}")
     return list_images(name, all, filters, token)
 
 
-@docker_router.post(Endpoints.DOCKER_REGISTRY_LOGIN)
+@image_router.post(Endpoints.DOCKER_REGISTRY_LOGIN)
 def dockerhub_login_service(username: str, password: str, token: str = Depends(oauth2_scheme)):
     logger.info(f"User is attempting to login to DockerHub with username: {username}")
     return dockerhub_login(username, password, token)
 
 
-@docker_router.post(Endpoints.IMAGE_PUSH)
+@image_router.post(Endpoints.IMAGE_PUSH)
 def push_image_service(local_tag: str, remote_repo: str, token: str = Depends(oauth2_scheme)):
     logger.info(f"User is attempting to push image with tag: {local_tag} to remote repository: {remote_repo}")
     return push_image(local_tag, remote_repo, token)
 
 
-@docker_router.post(Endpoints.IMAGE_PULL)
+@image_router.post(Endpoints.IMAGE_PULL)
 def pull_image_service(repository: str, local_tag: str = None, token: str = Depends(oauth2_scheme)):
     logger.info(f"User is attempting to pull image from repository: {repository}")
     return pull_image(repository, local_tag, token)
 
 
-@docker_router.delete(Endpoints.IMAGE_DELETE)
+@image_router.delete(Endpoints.IMAGE_DELETE)
 def remove_image_service(image_name: str, params: ImageRemoveRequest, token: str = Depends(oauth2_scheme)):
     logger.info(f"User is attempting to remove image with name: {image_name}")
     return remove_image(image_name, params, token)
